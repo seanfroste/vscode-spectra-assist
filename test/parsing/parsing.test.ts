d2adaa79876ec7e4fdf992208af93b88c2320b74
Sean Froste <sk30102k+github@gmail.com>
2024-08-10 00:32:49 +1000

    prettified with prettier, tab size 2

--- test/parsing/parsing.test.ts
+++ test/parsing/parsing.test.ts
@@ -6,55 +6,66 @@ import { createSpectraInServices } from "../../src/language/spectra-in-module.js
 import { Model, isModel } from "../../src/language/generated/ast.js";
 
 let services: ReturnType<typeof createSpectraInServices>;
-let parse:    ReturnType<typeof parseHelper<Model>>;
+let parse: ReturnType<typeof parseHelper<Model>>;
 let document: LangiumDocument<Model> | undefined;
 
 beforeAll(async () => {
-    services = createSpectraInServices(EmptyFileSystem);
-    parse = parseHelper<Model>(services.SpectraIn);
+  services = createSpectraInServices(EmptyFileSystem);
+  parse = parseHelper<Model>(services.SpectraIn);
 
-    // activate the following if your linking test requires elements from a built-in library, for example
-    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+  // activate the following if your linking test requires elements from a built-in library, for example
+  // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
 });
 
-describe('Parsing tests', () => {
-
-    test('parse simple model', async () => {
-        document = await parse(`
+describe("Parsing tests", () => {
+  test("parse simple model", async () => {
+    document = await parse(`
             person Langium
             Hello Langium!
         `);
 
-        // check for absensce of parser errors the classic way:
-        //  deacivated, find a much more human readable way below!
-        // expect(document.parseResult.parserErrors).toHaveLength(0);
+    // check for absensce of parser errors the classic way:
+    //  deacivated, find a much more human readable way below!
+    // expect(document.parseResult.parserErrors).toHaveLength(0);
 
-        expect(
-            // here we use a (tagged) template expression to create a human readable representation
-            //  of the AST part we are interested in and that is to be compared to our expectation;
-            // prior to the tagged template expression we check for validity of the parsed document object
-            //  by means of the reusable function 'checkDocumentValid()' to sort out (critical) typos first;
-            checkDocumentValid(document) || s`
+    expect(
+      // here we use a (tagged) template expression to create a human readable representation
+      //  of the AST part we are interested in and that is to be compared to our expectation;
+      // prior to the tagged template expression we check for validity of the parsed document object
+      //  by means of the reusable function 'checkDocumentValid()' to sort out (critical) typos first;
+      checkDocumentValid(document) ||
+        s`
                 Persons:
-                  ${document.parseResult.value?.persons?.map(p => p.name)?.join('\n  ')}
+                  ${document.parseResult.value?.persons
+                    ?.map((p) => p.name)
+                    ?.join("\n  ")}
                 Greetings to:
-                  ${document.parseResult.value?.greetings?.map(g => g.person.$refText)?.join('\n  ')}
+                  ${document.parseResult.value?.greetings
+                    ?.map((g) => g.person.$refText)
+                    ?.join("\n  ")}
             `
-        ).toBe(s`
+    ).toBe(s`
             Persons:
               Langium
             Greetings to:
               Langium
         `);
-    });
+  });
 });
 
 function checkDocumentValid(document: LangiumDocument): string | undefined {
-    return document.parseResult.parserErrors.length && s`
+  return (
+    (document.parseResult.parserErrors.length &&
+      s`
         Parser errors:
-          ${document.parseResult.parserErrors.map(e => e.message).join('\n  ')}
-    `
-        || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
-        || !isModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a '${Model}'.`
-        || undefined;
+          ${document.parseResult.parserErrors
+            .map((e) => e.message)
+            .join("\n  ")}
+    `) ||
+    (document.parseResult.value === undefined &&
+      `ParseResult is 'undefined'.`) ||
+    (!isModel(document.parseResult.value) &&
+      `Root AST object is a ${document.parseResult.value.$type}, expected a '${Model}'.`) ||
+    undefined
+  );
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,19 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://vitest.dev/config/
  */
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        // coverage: {
-        //     provider: 'v8',
-        //     reporter: ['text', 'html'],
-        //     include: ['src'],
-        //     exclude: ['**/generated'],
-        // },
-        deps: {
-            interopDefault: true
-        },
-        include: ['**/*.test.ts']
-    }
+  test: {
+    // coverage: {
+    //     provider: 'v8',
+    //     reporter: ['text', 'html'],
+    //     include: ['src'],
+    //     exclude: ['**/generated'],
+    // },
+    deps: {
+      interopDefault: true,
+    },
+    include: ["**/*.test.ts"],
+  },
 });
